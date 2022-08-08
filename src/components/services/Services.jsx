60f5cfb409e3a3633f874015f6300c923d48442f
George Kibe <georgekibewambui@gmail.com>
2022-08-08 16:53:44 +0300

    services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,77 @@
 import React from 'react'
+import {BiCheck} from "react-icons/bi"
+
 import "./services.css"
 
 const Services = () => {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>What I offer</h5>
+      <h2>Services </h2>
+
+      <div className="container services__container">
+        {/* Web Development */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"/> 
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/> 
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/> 
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+        </article>
+        {/* Mobile Development */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Mobile Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"/> 
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/> 
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/> 
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+        </article>
+        {/* Data Engineering */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Data Engineering</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"/> 
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/> 
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/> 
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
