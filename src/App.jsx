4190945aad0b15a24e37c6fbefed9191cf2a6748
George-Kibe <georgekibewambui@gmail.com>
2023-06-12 23:05:51 +0300

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import PortfolioPage from "./pages/portfolio/PortfolioPage";
-import ImagesPage from "./pages/imagesweb/ImagesPage";
 
 function App() {
   return (
     <div id="home">
-      <Router>
-        <Routes>
-          <Route path="/george-portfolio" element={<PortfolioPage />} />
-          <Route path="/images" element={<ImagesPage />} />
-        </Routes>
-      </Router>
+      <PortfolioPage />
     </div>
   );
 }
