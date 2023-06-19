667fa639fc69e52b5f241c2b0ddeae313b992568
George-Kibe <georgekibewambui@gmail.com>
2023-06-19 20:20:28 +0300

    error pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -20,17 +20,17 @@ const AboutPage = () => {
         <AnimatedText text={"Passion Fuels Purpose! Why Fuels How!"} />
         <div className="flex flex-col md:grid w-full grid-cols-8 gap-16">
             <div className="col-span-3 flex flex-col items-start justify-start">
-              <h2 className="mb-4 text-lg font-bold uppercase">About Me</h2>
-              <p className="font-medium text-justify my-4">
+              <h2 className="mb-2 md:mb-4 text-lg font-bold uppercase">About Me</h2>
+              <p className="font-medium text-justify my-2 md:my-4">
                 - Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
                 and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
                 new and innovative ways to bring my clients' visions to life.
               </p>
-              <p className="font-medium text-justify my-4">
+              <p className="font-medium text-justify my-2 md:my-4">
                 - I believe that design is about more than just making things look pretty – it's about solving problems and 
                 creating intuitive, enjoyable experiences for users. 
               </p>
-              <p className="font-medium text-justify my-4">
+              <p className="font-medium text-justify my-2 md:my-4">
                 - Whether I'm working on a website, mobile app, or 
                 other digital product, I bring my commitment to design excellence and user-centered thinking to 
                 every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
